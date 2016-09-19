1f1c0c00f5b801593e665933740dd9d65678d1d0
Mauricio Monsivais <mmonsivais@unm.edu>
2016-09-19 15:53:01 -0600

    added pathTaken to Zombie refactored creature, player, and zombie updated README controls

--- src/entities/Creature.java
+++ src/entities/Creature.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 
 /**
  * @author Ben Matthews
- * Class that contains fields and methods
- * that are common between all creatures
- * (player + monsters)
+ *         Class that contains fields and methods
+ *         that are common between all creatures
+ *         (player + monsters)
  */
 public abstract class Creature extends Entity
 {
@@ -20,31 +20,34 @@ public abstract class Creature extends Entity
   private double distanceTraveled;
 
   ArrayList<Point2D.Double> pathTaken;
-  
+  boolean enganged = false;
+
   /**
    * Get distance that the zombie has traveled and
-   * play a sound effect if the zombie is close enough 
+   * play a sound effect if the zombie is close enough
    * to the player.
    */
   void updateDistance()
   {
     distanceTraveled += calculateDistance();
-    if (distanceTraveled > stepDistance){
+    if (distanceTraveled > stepDistance)
+    {
       distanceTraveled = 0;
       stepSound();
     }
   }
+
   /**
    * Plays sound effects for player and zombies.
    */
   public abstract void stepSound();
-  
+
   /**
    * For zombie and player, calculates distance between the last
    * and current locations.
-   * @return
-   *      The square root of the sum of the squares of deltaX and
-   *      deltaZ. 
+   *
+   * @return The square root of the sum of the squares of deltaX and
+   * deltaZ.
    */
   public abstract double calculateDistance();
 }
