30928d89cb362e0a4280f10d3f954f9fef6087a0
JavierZazueta <javierzazueta23@gmail.com>
2016-10-10 15:57:41 -0600

    fixed chainsaw angle

--- src/entities/Chainsaw.java
+++ src/entities/Chainsaw.java
@@ -2,20 +2,19 @@ package entities;
 
 import com.interactivemesh.jfx.importer.obj.ObjImportOption;
 import com.interactivemesh.jfx.importer.obj.ObjModelImporter;
-import com.sun.org.apache.xml.internal.dtm.Axis;
-import javafx.geometry.Point3D;
 import javafx.scene.Node;
 import javafx.scene.transform.Rotate;
 
 /**
- * Created by javier on 10/6/2016.
+ * @author Javier
+ * @author Mauricio
  */
 public class Chainsaw extends Entity
 {
   private final double SCALE = .009;
   private final double Y_TRANSLATE = 11.95;
-  private final int xOffset = 42;
-  private final int zOffset = 4;
+  private final double X_OFFSET = 41.7;
+  private final int Z_OFFSET = 4;
 
   private EntityManager entityManager;
   private ObjModelImporter o;
@@ -39,12 +38,12 @@ public class Chainsaw extends Entity
       in[i].setScaleY(SCALE);
       in[i].setScaleZ(SCALE);
       in[i].setRotationAxis(Rotate.Y_AXIS);
-      in[i].setRotate(40);
-      in[i].setTranslateX(xOffset + xTranslate);
+      in[i].setRotate(80);
+      in[i].setTranslateX(X_OFFSET + xTranslate);
       in[i].setTranslateY(Y_TRANSLATE);
-      in[i].setTranslateZ(zOffset + zTranslate);
-      in[i].setRotationAxis(Rotate.Z_AXIS);
-      in[i].setRotate(15);
+      in[i].setTranslateZ(Z_OFFSET + zTranslate);
+//      in[i].setRotationAxis(Rotate.Z_AXIS);
+//      in[i].setRotate(15);
     }
     o.close();
   }
