bab41987c5ec0ebd17093b75253b547e11966a54
JavierZazueta <javierzazueta23@gmail.com>
2016-10-10 12:49:22 -0600

    added the chainsaw

--- src/entities/Chainsaw.java
+++ src/entities/Chainsaw.java
@@ -19,16 +19,16 @@ public class Chainsaw extends Entity
     o = new ObjModelImporter();
     o.setOptions(ObjImportOption.NONE);
     //o.read("Resources/Meshes/Sofa/Sofa.obj");
-    o.read("Resources/Meshes/Chainsaw/chainsaw_no_Modifiere.obj");
+    o.read("Resources/Meshes/Chainsaw/chainsaw_no_Modifiere1.obj");
     in = o.getImport();
     for(int i = 0; i < in.length; i++)
     {
       in[i].setRotationAxis(Rotate.Y_AXIS);
-      in[i].setScaleX(.5);
-      in[i].setScaleY(.5);
-      in[i].setScaleZ(.5);
+//      in[i].setScaleX(.5);
+//      in[i].setScaleY(.5);
+//      in[i].setScaleZ(.5);
       in[i].setTranslateX(3);
-      in[i].setTranslateY(0);
+      in[i].setTranslateY(.15);
       in[i].setTranslateZ(4);
     }
     o.close();
