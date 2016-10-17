3dd8a0af2895875154072580eaef45a7141e9ce5
JavierZazueta <javierzazueta23@gmail.com>
2016-10-17 02:06:06 -0600

    Merge branch 'master' of C:\Users\javie\Desktop\CS351\ZombieHouse-CS351(fin) with conflicts.

--- src/entities/EntityManager.java
+++ src/entities/EntityManager.java
@@ -4,17 +4,15 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import com.interactivemesh.jfx.importer.obj.ObjImportOption;
-import com.interactivemesh.jfx.importer.obj.ObjModelImporter;
 import game_engine.Attributes;
 import game_engine.Scenes;
 import game_engine.ZombieHouse3d;
 import gui.Main;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
 import javafx.scene.shape.Shape3D;
 import levels.Tile;
 import sounds.Sound;
@@ -53,7 +51,6 @@ public class EntityManager
   public Scenes scenes;
   private Group root;
   public Main main;
-
   public boolean masterZombieSpawn = false;
   public AtomicBoolean gameIsRunning = new AtomicBoolean(true);
   private Tile[][] gameBoard;
@@ -61,7 +58,6 @@ public class EntityManager
   private MasterZombieDecision masterDecision;
   private ZombieDecision zombieDecision;
   private int zombiePathIndex = 0;
-  private int collisionTicks = 59;
 
   /**
    * Constructor for EntityManager.
@@ -123,48 +119,37 @@ public class EntityManager
   /**
    * Collision detection for 3D player objects.
    *
-   * @param player The shape that represents the player.
+   * @param player The shape that represents the zombie.
    * @return True if there is a collision. False if there isn't.
    */
-  boolean checkPlayerCollision(Shape3D player)
+  boolean checkPlayerCollision(Shape3D player, Shape3D chainsaw)
   {
     for (Zombie zombie : zombies)
     {
-      // if at last index of zombie's path he is dead, remove. (Used for ghost killing zombie)
       if (zombiePathIndex == zombie.pathTaken.size() - 1 && zombie.pathTaken.get(zombiePathIndex).getIsDead() == 1)
       {
-        System.out.println("removes zombie");
         zombie.setDead(true);
         deadZombies.add(zombie);
         zombies.remove(zombie);
         root.getChildren().removeAll(zombie.getMesh());
         return false;
-
       }
-      //if player and zombie are colliding
-      else if (player.getBoundsInParent().intersects(zombie.ZOMBIE_HITBOX.getBoundsInParent()))
+      else if (chainsaw.getBoundsInParent().intersects(zombie.ZOMBIE_HITBOX.getBoundsInParent()))
       {
-        //if player is attacking and facing zombie
         if (this.player.attacking && (this.player.angle - zombie.angle > -300 && this.player.angle - zombie.angle < 300))
         {
-          System.out.println(collisionTicks);
-          if (!zombie.hasPath())
+          if (!zombie.isEngaged() || !zombie.isDead())
           {
+            System.out.println("zombie damaged");
             zombie.takeHealth();
           }
-          else
+          else if (zombie.isEngaged() && zombie.isDead())
           {
-            collisionTicks++;
-            if (collisionTicks == 60)
-            {
-              bifurcate(zombie);
-              collisionTicks = 0;
-            }
+//            createZombie(zombie);
           }
         }
         if (zombie.getHealth() <= 0)
         {
-          System.out.println("killed");
           byte dead = 1;
           //need to blow up the zombie
           zombie.pathTaken.get(zombie.pathTaken.size() - 1).setIsDead(dead);
@@ -173,19 +158,24 @@ public class EntityManager
           zombies.remove(zombie);
           root.getChildren().removeAll(zombie.getMesh());
         }
-        if (zombie.hasPath()) return false;
+        if (zombie.isEngaged() && zombie.getTakeHealth() == 1) return false;
+        //return true;
+      }
+       if (player.getBoundsInParent().intersects(zombie.ZOMBIE_HITBOX.getBoundsInParent()))
+      {
         return true;
       }
     }
     return false;
   }
 
-  private void bifurcate(Zombie zombie)
+  private void createZombie(Zombie zombie)
   {
     Zombie newZombie = new Zombie(gameBoard[zombie.getRow()][zombie.getCol()], this);
     zombies.add(newZombie);
-    newZombie.setMesh(ZombieHouse3d.loadMeshViews("Resources/Meshes/Feral_Ghoul/Feral_Ghoul.obj"));
-    root.getChildren().addAll(newZombie.getMesh());
+    zombie.setMesh(ZombieHouse3d.loadMeshViews("Resources/Meshes/Feral_Ghoul/Feral_Ghoul.obj"));
+
+    root.getChildren().addAll(zombie.getMesh());
   }
 
   /**
